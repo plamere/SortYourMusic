3b246d18f62e40576192fbfd948092a0b1591b04
Paul Lamere <Paul.Lamere@gmail.com>
2015-07-15 15:32:17 +0200

    Minor UI tweaks and an ack for Sonneveld

--- web/config.js
+++ web/config.js
@@ -5,4 +5,5 @@ var API_HOST = 'http://labs2.echonest.com/SortYourMusic'
 
 var SPOTIFY_CLIENT_ID = '31469b011d4941bf8dd4ac9cf8495bac';
 var SPOTIFY_REDIRECT_URI = 'http://localhost:8235/SortYourMusic/';
+var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
 var SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/SortYourMusic/';
