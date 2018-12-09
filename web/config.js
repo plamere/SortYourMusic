87a78b40afcaf2fbe3c4ef3c14e4c104cbd3ca2f
Paul Lamere <paull@spotify.com>
2018-12-09 08:43:22 -0500

    Added RND column

--- web/config.js
+++ web/config.js
@@ -3,4 +3,4 @@
 var SPOTIFY_CLIENT_ID = '31469b011d4941bf8dd4ac9cf8495bac';
 var SPOTIFY_REDIRECT_URI = 'http://localhost:8235/SortYourMusic/';
 var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
-var SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/SortYourMusic/';
+var SPOTIFY_REDIRECT_URI = 'http://sortyourmusic.playlistmachinery.com/';
