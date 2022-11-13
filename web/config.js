9f86e6c08450fb463e983d092ce3f46670e6d601
Paul Lamere <paull@spotify.com>
2022-11-13 07:14:41 -0500

    Added artist separation sort
    Also, improved look

--- web/config.js
+++ web/config.js
@@ -1,6 +1,6 @@
 "use strict";
 
 var SPOTIFY_CLIENT_ID = '31469b011d4941bf8dd4ac9cf8495bac';
-var SPOTIFY_REDIRECT_URI = 'http://localhost:8235/SortYourMusic/';
-var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
 var SPOTIFY_REDIRECT_URI = 'http://sortyourmusic.playlistmachinery.com/';
+var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
+
